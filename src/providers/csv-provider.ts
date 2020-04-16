8f5bad44711a68852582de7a807acb75858dcefa
aviv <av>
2020-04-16 14:15:18 +0300

    add csv reader

---
+++ src/providers/csv-provider.ts
@@ -0,0 +1,18 @@
+import csv from 'csv-parser'
+import fs from 'fs'
+const results = [];
+
+function read() {
+
+} 
+
+fs.createReadStream('data.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    console.log(results);
+    // [
+    //   { NAME: 'Daffy Duck', AGE: '24' },
+    //   { NAME: 'Bugs Bunny', AGE: '22' }
+    // ]
+  });
