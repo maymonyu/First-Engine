a12d269dca5a4c9183c1651384e89a5b1e38793d
Yuval Maymon <maymonyu@post.bgu.ac.il>
2020-04-19 18:05:18 +0300

    removing automated linter

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
-import {Cluster, Itur} from './types';
-import {readClusters, readIturim} from './dataProviders/csvProvider';
+import {Cluster, Itur, Constitution} from './types';
+import {readClusters, readIturim, readConstitution} from './dataProviders/csvProvider';
 
 const processData = async () => {
   const clusters: Cluster[] = await readClusters();
   const iturim: Itur[] = await readIturim();
+  const constitution: Constitution = await readConstitution();
 };
 
 processData();
