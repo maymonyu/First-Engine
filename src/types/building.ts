8358c3d0b9992cdf093596e88dd2dc76f11e8ae4
Or Mauda <ormauda@gmail.com>
2020-04-21 13:36:03 +0300

    change types

--- src/types/building.ts
+++ src/types/building.ts
@@ -1,9 +1,12 @@
-import {Coordinate} from './';
+import {Coordinate, Itur} from './';
 
 export class Building {
     coordinates: Coordinate[];
+    iturim?: Itur[];
+    score: number;
 
     constructor(coordinates: Coordinate[]) {
         this.coordinates = coordinates;
+        this.score = 0;
     }
 }
