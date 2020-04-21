07314a2307c4bd6d19b3fd25fd749269b3812a66
Elados <elad619@gmail.com>
2020-04-21 19:34:09 +0300

    some tests cr fixes

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,30 +1,25 @@
-import {Cluster} from './types/cluster';
-import {Coordinate} from './types/coordinate';
-import {CrossChecker} from './modules/cross-checker/cross-checker';
-import {Itur} from './types/itur';
-import {Building} from './types';
-
-const crossChecker = new CrossChecker();
+import {Cluster, Coordinate, Itur, Building} from './types/index';
+import {crossClusterWithIturim} from './modules/cross-checker/cross-checker';
 
 function createIturExample(coordinate: Coordinate): Itur {
     return {
-        'index': 123,
-        'location': coordinate,
-        'profession': 'assassin',
-        'tabuOwner': 'moshe',
-        'names': 'bla',
+        index: 123,
+        location: coordinate,
+        profession: 'assassin',
+        tabuOwner: 'moshe',
+        names: 'bla',
     };
 }
 
 function createClusterExample(geoBuildings: Building[]): Cluster {
     return {
-        'id': 'clusterTest1',
-        'clusteringQuality': 'low',
-        'identification': 'sdsds',
-        'minStaying': 2,
-        'profession': 'wizard',
-        'numberOfBuildings': 1,
-        'geoBuildings': geoBuildings,
+        id: 'clusterTest1',
+        clusteringQuality: 'low',
+        identification: 'sdsds',
+        stayingInterval: {minTimeinHours: 2, maxTimeInHours: 3},
+        profession: 'wizard',
+        numberOfBuildings: 1,
+        geoBuildings: geoBuildings,
     };
 };
 
@@ -36,7 +31,7 @@ test('Set1: basic itur inside cluster', () => {
     const cluster = createClusterExample([building]);
 
     const itur = createIturExample({latitude: 31.3016, longitude: 34.2752});
-    const crossBuildings = crossChecker.crossClusterWithIturim(cluster, [itur]).geoBuildings;
+    const crossBuildings = crossClusterWithIturim(cluster, [itur]).geoBuildings;
 
     expect(crossBuildings.length).toBe(1);
     expect(crossBuildings[0].iturim ? crossBuildings[0].iturim.length : -1).toBe(1);
@@ -52,7 +47,7 @@ test('Set1: basic itur not inside cluster', () => {
     const cluster = createClusterExample([building]);
 
     const itur = createIturExample({latitude: 38.8888, longitude: 34.2752});
-    const crossBuildings = crossChecker.crossClusterWithIturim(cluster, [itur]).geoBuildings;
+    const crossBuildings = crossClusterWithIturim(cluster, [itur]).geoBuildings;
 
     expect(crossBuildings.length).toBe(1);
     expect(crossBuildings[0].iturim ? 1 : 0).toBe(0);
@@ -72,7 +67,7 @@ test('Set2: itur with 2 buildings when in the second one', () => {
     const cluster = createClusterExample([building1, building2]);
 
     const itur = createIturExample({latitude: 31.3016, longitude: 34.2752});
-    const crossBuildings = crossChecker.crossClusterWithIturim(cluster, [itur]).geoBuildings;
+    const crossBuildings = crossClusterWithIturim(cluster, [itur]).geoBuildings;
 
     expect(crossBuildings.length).toBe(2);
     expect(crossBuildings[0].iturim ? 1 : 0).toBe(0);
@@ -95,7 +90,7 @@ test('Set2: itur that does not appear in both buildings', () => {
     const cluster = createClusterExample([building1, building2]);
 
     const itur = createIturExample({latitude: 33.3016, longitude: 34.2752});
-    const crossBuildings = crossChecker.crossClusterWithIturim(cluster, [itur]).geoBuildings;
+    const crossBuildings = crossClusterWithIturim(cluster, [itur]).geoBuildings;
 
     expect(crossBuildings.length).toBe(2);
     expect(crossBuildings[0].iturim ? 1 : 0).toBe(0);
@@ -112,7 +107,7 @@ test('Set3: changing location , few iturim , polygon have 4 coordinates', () => 
 
     const firstItur = createIturExample({latitude: 32.071291, longitude: 34.805200}); // inside poly
     const secondItur = createIturExample({latitude: 31.30161, longitude: 34.2752}); // not inside
-    const crossBuildings = crossChecker.crossClusterWithIturim(cluster, [firstItur, secondItur]).geoBuildings;
+    const crossBuildings = crossClusterWithIturim(cluster, [firstItur, secondItur]).geoBuildings;
 
     expect(crossBuildings.length).toBe(1);
     expect(crossBuildings[0].iturim ? crossBuildings[0].iturim.length : -1).toBe(1);
@@ -133,7 +128,7 @@ test('Set4: few iturim inside the same building', () => {
     const secondItur = createIturExample({latitude: 32.071273, longitude: 34.805088});
     const thirdItur = createIturExample({latitude: 32.071218, longitude: 34.805313});
     const fourthItur = createIturExample({latitude: 32.071332, longitude: 34.805222});
-    const crossBuildings = crossChecker.crossClusterWithIturim(
+    const crossBuildings = crossClusterWithIturim(
             cluster,
             [firstItur, secondItur, thirdItur, fourthItur],
     ).geoBuildings;
@@ -143,7 +138,7 @@ test('Set4: few iturim inside the same building', () => {
     expect(crossBuildings[0].coordinates.length).toBe(4);
 });
 
-test('Set4: few iturim around the building none should be considered', () => {
+test('Set4: few iturim outside the building none should be considered', () => {
     const point1 = new Coordinate(32.071341, 34.805351);
     const point2 = new Coordinate(32.071382, 34.805109);
     const point3 = new Coordinate(32.071223, 34.805077);
@@ -155,7 +150,7 @@ test('Set4: few iturim around the building none should be considered', () => {
     const secondItur = createIturExample({latitude: 32.071195, longitude: 34.804964});
     const thirdItur = createIturExample({latitude: 32.071136, longitude: 34.805431});
     const fourthItur = createIturExample({longitude: 32.071364, latitude: 34.805447});
-    const crossBuildings = crossChecker.crossClusterWithIturim(
+    const crossBuildings = crossClusterWithIturim(
             cluster,
             [firstItur, secondItur, thirdItur, fourthItur],
     ).geoBuildings;
@@ -185,7 +180,7 @@ test('Set5: few iturim split between 2 buildings', () => {
     const fourthItur = createIturExample({latitude: 32.071086, longitude: 34.805619}); // in second
     const fifthItur = createIturExample({latitude: 32.071182, longitude: 34.805437}); // not in any
 
-    const crossBuildings = crossChecker.crossClusterWithIturim(
+    const crossBuildings = crossClusterWithIturim(
             cluster, [firstItur, secondItur, thirdItur, fourthItur, fifthItur]).geoBuildings;
 
     expect(crossBuildings.length).toBe(2);
